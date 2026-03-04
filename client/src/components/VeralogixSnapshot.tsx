a9269d8ae5e29abbdceefae8131bbffe0fde5435
rmoffett99 <rashadmoffett@gmail.com>
2026-03-04 13:46:13 -0600

    Update VeralogixSnapshot.tsx with deprecation notice

--- client/src/components/VeralogixSnapshot.tsx
+++ client/src/components/VeralogixSnapshot.tsx
@@ -1,1 +1,15 @@
-This is a placeholder to retrieve the file SHA.
+/**
+ * @deprecated This file has been moved to client/src/veralogix/pages/SnapshotPage.tsx
+ * 
+ * This component is no longer maintained in this location.
+ * All Veralogix-related code has been consolidated under the client/src/veralogix/ directory.
+ * 
+ * Migration Notes:
+ * - Old import: import VeralogixSnapshot from "./components/VeralogixSnapshot"
+ * - New import: import { SnapshotPage } from "@/veralogix"
+ * - New location: client/src/veralogix/pages/SnapshotPage.tsx
+ * 
+ * Please update your imports accordingly.
+ */
+
+export { default } from '../../../veralogix/pages/SnapshotPage';
